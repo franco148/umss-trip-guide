f306eb1f64cc73c22d53924fcadbd81de3865b9f
franco-extreme <franck_ral@hotmail.com>
2020-03-17 18:13:14 -0400

    utg-security-features: Applied security features into the project.

--- src/main/java/com/umss/projects/controller/UserRestController.java
+++ src/main/java/com/umss/projects/controller/UserRestController.java
@@ -1,6 +1,5 @@
 package com.umss.projects.controller;
 
-import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
